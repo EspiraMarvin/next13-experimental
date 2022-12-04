6fe84b71f28bc50717317857f3e48ad978c53f4c
marvin espira <espiramarvin@gmail.com>
2022-12-04 13:30:24 +0300

    updates

--- app/issues/layout.tsx
+++ app/issues/layout.tsx
@@ -1,16 +1,20 @@
+import { PrismaClient } from "@prisma/client"
 import React from "react"
 import IssueList from "../issues/IssueList"
 import classes from "./layout.module.css"
 
-export default function IssuesLayout({
+export default async function IssuesLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const prisma = new PrismaClient()
+  const issues = await prisma.issue.findMany()
+
   return (
     <div className={classes.layout}>
       <aside className={classes.sidebar}>
-        <IssueList issues={[]} />
+        <IssueList issues={issues} />
       </aside>
       {children}
     </div>
