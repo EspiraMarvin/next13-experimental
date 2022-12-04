e16340bc00a55ab6bbb7dfc4ab85617f0a233587
marvin espira <espiramarvin@gmail.com>
2022-12-04 14:24:30 +0300

    feat: updates

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,9 +3,5 @@ import React from "react"
 type Props = {}
 
 export default function page({}: Props) {
-  return (
-    <div>
-      <h1>the issues page</h1>
-    </div>
-  )
+  return <p>please select an issue</p>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>The starting page</h1>
-    </div>
-  )
+export default function IssuesPage() {
+  return <h1>The starting page</h1>
 }
