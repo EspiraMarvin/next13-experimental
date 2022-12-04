716c8cb63fd44e61516956d1e08e12710be0b7a6
marvin espira <espiramarvin@gmail.com>
2022-12-04 11:27:58 +0300

    feat: updates

---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <h1>the issues page</h1>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import "./globals.css"
+import MainNavigation from "../components/ui/MainNavigation"
 
 export default function RootLayout({
   children,
@@ -12,7 +13,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
